b965789d963d7f272c4b21151cf917a42a791efb
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-06-26 15:40:10 +0700

    create form middleware

--- index.js
+++ index.js
@@ -3,7 +3,39 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi there");
+  res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confirmation" />
+            <button>Sign up</button>
+        </form>
+    </div>
+  `);
+});
+
+const bodyParser = (req, res, next) => {
+  if (req.method === "POST") {
+    req.on("data", (data) => {
+      const parsed = data.toString("utf8").split("&");
+      const formData = {};
+
+      for (let pair of parsed) {
+        const [key, value] = pair.split("=");
+        formData[key] = value;
+      }
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post("/", bodyParser, (req, res) => {
+  console.log(req.body);
+  res.send("Account created!!!");
 });
 
 app.listen(3000, () => {
