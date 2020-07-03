26fae17b44c2b55cc129a179d685d15457b195ae
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-07-03 12:31:48 +0700

    finish user repo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const usersRepo = require("./repositories/users");
 
 const app = express();
 
@@ -18,8 +19,19 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Password must match");
+  }
+
   res.send("Account created!!!");
 });
 
--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepository {
   constructor(filename) {
@@ -24,21 +25,65 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
+
     const records = await this.getAll();
     records.push(attrs);
 
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecord = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecord);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+
+      if (found) {
+        return record;
+      }
+    }
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository("users.json");
-
-  await repo.create({ email: "test@test.com", password: "password" });
-
-  const users = await repo.getAll();
-
-  console.log(users);
-};
-
-test();
+module.exports = new UsersRepository("users.json");
