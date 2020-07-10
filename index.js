ee93730dc7ce5e22d93aa488d9ea25b83bf68152
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-07-10 15:40:08 +0700

    refacto project structur

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
-const usersRepo = require("./repositories/users");
+const authRouter = require("./routes/admin/auth");
 
 const app = express();
 
@@ -12,79 +12,7 @@ app.use(
   })
 );
 
-app.get("/signup", (req, res) => {
-  res.send(`
-    <div>
-    Your id is:  ${req.session.userId} 
-        <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <input name="passwordConfirmation" placeholder="password confirmation" />
-            <button>Sign up</button>
-        </form>
-    </div>
-  `);
-});
-
-app.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-
-  if (existingUser) {
-    return res.send("Email in use");
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send("Password must match");
-  }
-
-  const user = await usersRepo.create({ email, password });
-
-  req.session.userId = user.id;
-
-  res.send("Account created!!!");
-});
-
-app.get("/signout", (req, res) => {
-  req.session = null;
-  res.send("You are logged out");
-});
-
-app.get("/signin", (req, res) => {
-  res.send(`
-   <div>
-        <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <button>Sign in</button>
-        </form>
-    </div>
-`);
-});
-
-app.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await usersRepo.getOneBy({ email });
-
-  if (!user) {
-    return res.send("Email not found");
-  }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-
-  if (!validPassword) {
-    return res.send("Invalid password");
-  }
-
-  req.session.userId = user.id;
-
-  res.send("You are signed in");
-});
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
