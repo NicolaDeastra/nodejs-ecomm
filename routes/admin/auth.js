c9323571aeba1cb4df8ae3e02748db4ea227afa8
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-07-19 14:37:46 +0700

    create error handle middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+const { check } = require("express-validator");
 
+const { handleErrors } = require("./middleware");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -21,13 +22,8 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
     const { email, password, passwordConfirmation } = req.body;
     const user = await usersRepo.create({ email, password });
 
@@ -50,13 +46,8 @@ router.post(
   "/signin",
 
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { validationResult } = require("express-validator");
 const multer = require("multer");
 
+const { handleErrors } = require("./middleware");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
@@ -17,11 +17,14 @@ router.get("/admin/products/new", (req, res) => {
 
 router.post(
   "/admin/products/new",
-  [requireTitle, requirePrice],
   upload.single("image"),
-  (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
+  [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
+  async (req, res) => {
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
+
+    await productsRepo.create({ title, price, image });
 
     res.send("submitted");
   }
