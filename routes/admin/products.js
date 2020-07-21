dbc847b44d9d0ac678be03ec8d20ee1ffbc98fd4
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-07-21 13:09:34 +0700

    add delete product funtion

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -72,4 +72,10 @@ router.post(
   }
 );
 
+router.post('/admin/products/:id/delete', requireAuth, async (req,res) => {
+  await productsRepo.delete(req.params.id)
+
+  res.redirect('/admin/products')
+})
+
 module.exports = router;
