c697ea3f905056cd8ea5c89d965d0750ebaa01ee
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-07-25 14:17:17 +0700

    templating cart page

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+
+const cartShowTemplate = require("../views/cart/show");
 
 const router = express.Router();
 
@@ -28,6 +31,22 @@ router.post("/cart/products", async (req, res) => {
   });
 
   res.send("Product added to cart");
+});
+
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
 });
 
 module.exports = router;
